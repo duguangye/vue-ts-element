313d908d20e2ee0011a58cdea89f0d1a8cedcca1
hll <linlin.huang@wowjoy.cn>
2023-01-30 16:52:05 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <router-link to="/infiniteScrollTable">无限滚动的table</router-link>
+  </main>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ src/views/infiniteScroll/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <thead>
+  <tr>
+    <th>Month</th>
+    <th>Savings</th>
+  </tr>
+  </thead>
+  <tbody>
+  <tr v-for="item in list">
+    <td>{{item.name}}</td>
+    <td>¥{{item.price}}</td>
+  </tr>
+  </tbody>
+</template>
+
+<script setup lang="ts">
+import {reactive} from "vue";
+
+const list: Array<{ name: string, price: number }> = reactive([])
+for (let i = 0; i < 10; i++) {
+  list.push({
+    name: '日期' + i,
+    price: i
+  })
+}
+
+</script>
+
+<style  scoped>
+td{
+  line-height: 25px;
+  width: 120px;
+}
+</style>
